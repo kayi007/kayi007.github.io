6995ef9972fad50455eac24f8016b4ac704c83f8
Kathleen Leung <kathleenleungky@gmail.com>
2019-08-11 17:41:26 -0700

    added javascript and changed one project screenshot, added more project on portfolio page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-var forMore = $("<div>")
-forMore.text("click to play");
-$(".projImg").append(forMore);
-
-$(".projImg").on("mouseover", function(){
-    forMore.show();
-}).on("mouseout", function(){
-    forMore.hide();
+$("#pressButton").on("click", function(event){
+    event.preventDefault();
 });
