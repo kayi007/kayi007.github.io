dbbd0ab203fc7a4ba89a54438a669cfa552e70e2
Kathleen Leung <kathleenleungky@gmail.com>
2019-12-19 18:43:06 -0800

    created initial page & css, added loader to show portfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,3 +33,17 @@ $(function () {
         console.log('prev ' + prev);
     }
 });
+
+// $(document).ready(function(){
+//     var myVar;
+    
+//     function myFunction() {
+//         myVar = setTimeout(showPage, 3000);
+//     }
+    
+//     function showPage() {
+//         document.getElementById("loader").style.display = "none";
+//         document.getElementById("myDiv").style.display = "block";
+//     }
+// });
+
